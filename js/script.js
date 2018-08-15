384ab6f0d1031fcc139bcd584fe932e67a93b114
CodingwithShane <CodingwithShane@users.noreply.github.com>
2018-08-15 20:19:01 +1000

    Added the quotes array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-
+var quotes = [
+    {
+        quote: "The Way Get Started Is To Quit Talking And Begin Doing.",
+        source: "Walt Disney"
+    },
+    {
+        quote: "Talk is cheap. Show me the code.",
+        source: "Linus Torvalds",
+        citation: "Message to linux-kernel mailing list",
+        year: "2000-08-25"
+    },
+    {
+        quote: "Successful people do what unsuccessful people are not willing to do. Don't wish it were easier; wish you were better.",
+        source: "Jim Rohn"
+    },
+    {
+        quote: '<b>DL:</b>"How did you know so much about computers?"<br><b>Grace Hopper</b> - “I didn’t, it was the first one"',
+        source: "David Letterman interview with Grace Hopper.",
+        citation: "Late Night with David Letterman.",
+        year: 1986
+    },
+    {
+        quote: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+        source: "Bill Gates"
+    }
+]
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
