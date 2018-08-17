5bfd6b82b619b84b841dad7af99d92a6bc1b5424
CodingwithShane <CodingwithShane@users.noreply.github.com>
2018-08-17 19:52:17 +1000

    Finished attempt 1

--- js/script.js
+++ js/script.js
@@ -4,29 +4,34 @@
 var quotes = [
     {
         quote: "The Way Get Started Is To Quit Talking And Begin Doing.",
-        source: "Walt Disney"
+        source: "Walt Disney",
+        tag: "Motivation"
     },
     {
         quote: "Talk is cheap. Show me the code.",
         source: "Linus Torvalds",
+        tag: "Developer",
         citation: "Message to linux-kernel mailing list",
         year: "2000-08-25"
     },
     {
         quote: "Successful people do what unsuccessful people are not willing to do. Don't wish it were easier; wish you were better.",
-        source: "Jim Rohn"
+        source: "Jim Rohn",
+        tag: "Motivation"
     },
     {
         quote: '<b>DL:</b>"How did you know so much about computers?"<br><b>Grace Hopper</b> - “I didn’t, it was the first one"',
         source: "David Letterman interview with Grace Hopper",
+        tag: "Developer",
         citation: "Late Night with David Letterman",
         year: 1986
     },
     {
         quote: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
-        source: "Bill Gates"
+        source: "Bill Gates",
+        tag: "Developer"
     }
-]
+];
 
 // Function that gives a random color in RGB value.
 function randomBackground() {
@@ -48,7 +53,7 @@ function getRandomQuote(array) {
 function printQuote(randomQuote) {
     randomQuote = getRandomQuote();
     message = '<p class="quote">' + randomQuote.quote + '</p>';
-    message += '<p class="source">' + randomQuote.source;
+    message += '<p class="source">' + randomQuote.source + '\t \t || ' + randomQuote.tag;
     // If quote has citation and year then display them
     if (randomQuote.citation != undefined) {
         message += '<span class="citation">' + randomQuote.citation + '</span>';
@@ -56,7 +61,6 @@ function printQuote(randomQuote) {
             message += '<span class="year">' + randomQuote.year + '</span>';
         }
     }
-    message += '</p>'
 // First part changes the quotes while second changes the background color
     document.getElementById('quote-box').innerHTML = message;
     document.body.style.backgroundColor = randomBackground();
@@ -67,3 +71,8 @@ function printQuote(randomQuote) {
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// Runs the PrintQuote after 25 seconds
+setInterval(function() {
+    printQuote();
+}, 25000);
